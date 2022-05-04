be469a5e018ccbae47a84c3be3769a3bc48adafd
136312superwang <75189840+136312superwang@users.noreply.github.com>
2022-05-04 09:46:28 +0800

    1111

---
+++ src/main/java/com/atguigu/git/GitTest1.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 王长洪
+ * @creat 2022-05-04 9:45
+ */
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
