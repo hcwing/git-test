16c7c50dc67fb2a4cfa95a42130af433a4eac89d
136312superwang <75189840+136312superwang@users.noreply.github.com>
2022-05-04 09:28:36 +0800

    second commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 王长洪
+ * @creat 2022-05-04 7:53
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git hello java");
+    }
+}
