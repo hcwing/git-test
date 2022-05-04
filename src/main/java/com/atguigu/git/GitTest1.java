a0f84a7e79d52e1c6d725c149e9322afea8d8fc5
136312superwang <75189840+136312superwang@users.noreply.github.com>
2022-05-04 09:47:16 +0800

    2222

--- src/main/java/com/atguigu/git/GitTest1.java
+++ src/main/java/com/atguigu/git/GitTest1.java
@@ -7,5 +7,6 @@ package com.atguigu.git;
 public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
